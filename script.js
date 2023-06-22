6f9eab32039950114349301a19aadbd266211d6e
Sottaaa <attosrezende3@gmail.com>
2023-06-22 10:20:41 -0300

    Testes para capturar largura do usuario

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function pegarLargura() {
+const largura = window.innerWidth
+document.documentElement.clientWidth
+document.body.clientWidth;
+console.log(largura)
 
-    const largura = window.innerWidth
-    document.documentElement.clientWidth
-    document.body.clientWidth;
-
-    console.log(largura)
-
-
+function larg() {
+    let titulo = document.getElementById('teste')
+    titulo.innerHTML = largura
 }
+
