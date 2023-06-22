60c247e4773c6e81c6fe86296cd1c6afcbbdacc1
Sottaaa <attosrezende3@gmail.com>
2023-06-22 14:29:07 -0300

    Atualização do projeto

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-const largura = window.innerWidth
-document.documentElement.clientWidth
-document.body.clientWidth;
-console.log(largura)
 
-function larg() {
-    let titulo = document.getElementById('teste')
-    titulo.innerHTML = largura
-}
+
 
